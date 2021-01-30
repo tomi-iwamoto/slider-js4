45fdbecc3610d7fe44f5cbbfafd023ee4bff4265
Tomomi <58757939+tomi-design@users.noreply.github.com>
2021-01-30 02:26:03 -0500

    Changed the order of the image source in both functions so that whenever the button is clicked it doesn't go back to the first image each time

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@
     let images = [
         "img/one.jpg",
         "img/two.jpg",
-        "img/three.jpg"
+        "img/three.jpg",
+        "img/four.jpg"
     ]; 
 
     let nextButton = document.getElementById('next');
@@ -19,19 +20,32 @@
 
 //i want to go to the next image- access the next image - also define current image 
     function changeNextImg(){
+
+        if(i < images.length -1){//if its 0 or 1 index keep adding
+            i++;
+        } else {// if index is 2 then go back to index 0 
+            i=0;
+        }
         img.src = images[i];
 
-        if(i >= 0 && i < images.length -1){
-            i++;
-        }
-        if(i === images.length){
-            i = 0;
-        }
-        
     }
     
     nextButton.addEventListener('click', changeNextImg);
 
+    function changePrevImg(){
+       
+
+        if(i > 0 && i < images.length){//if i = 1 or 2
+            i--;
+        } else if(i === 0) {
+            i = 3; // if its num 1 go to image num 3
+        }
+        img.src = images[i];
+    }
+
+    prevButton.addEventListener('click', changePrevImg);
+
+      
 
 
   
